d8abe6c5cf0d04176c3fc7fa463f9cfc93e385c4
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2022-09-13 09:24:43 +0200

    new index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<div class="nav">
+				<img src="" alt="">
+				<a href=""></a>
+				<a href=""></a>
+				<a href=""></a>
+			</div>
+			<div class="header">
+				<h2>Lorem ipsum.</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate aliquam quas dolorum voluptatibus voluptate suscipit optio illum, magni sed exercitationem soluta necessitatibus maiores maxime at est culpa! Porro, deserunt!</p>
+			</div>
+		</header>
+		<main>
+		    
+		</main>
+		<footer>
+			
+		</footer>
+	</div>
+	
+</body>
+</html>
