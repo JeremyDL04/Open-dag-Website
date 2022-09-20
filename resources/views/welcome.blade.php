461d542656c2c30d0d6d92899103895e3ea01a5f
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2022-09-20 09:48:49 +0200

    Made the homepage with all necessary info

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+	    <link rel="icon" href="img/logo.png" />
+        <title>Open Dag</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <div class="nav">
+                    <img src="img/logo.png" width="100" alt="">
+                    <div class="links">
+                        <a href="">Home</a>
+                        <a href="">Informatie</a>
+                        <a href="">Contact</a>
+                    </div>
+                </div>
+                <div class="header">
+                    <h2>Lorem ipsum.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate aliquam quas dolorum voluptatibus voluptate suscipit optio illum, magni sed exercitationem soluta necessitatibus maiores maxime at est culpa! Porro, deserunt!</p>
+                </div>
+            </header>
+            <main>
+                <div class="main">
+                    <h2>Lorem ipsum.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate aliquam quas dolorum voluptatibus voluptate suscipit optio illum, magni sed exercitationem soluta necessitatibus maiores maxime at est culpa! Porro, deserunt!</p>
+                    <h1>Meer informatie bekijken?</h1>
+                    <a href="">Bekijk informatie</a>
+                </div>
+                <div class="main2">
+                    <h2>Lorem ipsum.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate aliquam quas dolorum voluptatibus voluptate suscipit optio illum, magni sed exercitationem soluta necessitatibus maiores maxime at est culpa! Porro, deserunt!</p>
+                </div>
+            </main>
+            <footer>
+                <p>&copy; 2022 Jeremy de Leeuw - student bij Curio.</p>
+            </footer>
+        </div>
+    </body>
+</html>
