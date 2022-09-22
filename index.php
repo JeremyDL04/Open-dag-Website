7a8c53024759a4feac3372d16a8f1c947d61c9e1
Jeremy de Leeuw <jeremydeleeuw@gmail.com>
2022-09-22 15:03:00 +0200

    Added information on index

--- index.php
+++ index.php
@@ -1,31 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<div class="wrapper">
-		<header>
-			<div class="nav">
-				<img src="" alt="">
-				<a href=""></a>
-				<a href=""></a>
-				<a href=""></a>
-			</div>
-			<div class="header">
-				<h2>Lorem ipsum.</h2>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate aliquam quas dolorum voluptatibus voluptate suscipit optio illum, magni sed exercitationem soluta necessitatibus maiores maxime at est culpa! Porro, deserunt!</p>
-			</div>
-		</header>
-		<main>
-		    
-		</main>
-		<footer>
-			
-		</footer>
-	</div>
-	
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+	    <link rel="icon" href="img/logo.png" />
+        <title>Open Dag</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <div class="nav">
+                    <img src="img/logo.png" width="100" alt="">
+                    <div class="links">
+                        <a href="">Home</a>
+                        <a href="">Informatie</a>
+                        <a href="">Contact</a>
+                    </div>
+                </div>
+                <div class="header">
+                    <h2>Over de opleiding</h2>
+                    <p>Op de opleiding Software Developer leer je alles over programmeren. Van kleine applicaties tot hele uitgebreide applicaties. Met alle kennis over Software Development kan je heel veel leuke en mooie applicaties maken. Ik vind het in ieder geval een heel leuke opleiding en ik zal er heel veel mee gaan doen in de toekomst.</p>
+                </div>
+				<div class="header">
+					<h2>Op school</h2>
+                    <p>Op onze opleiding is er een echt gevoel van kantoorwerk. Er wordt rustig en samenwerkend gewerkt. De opdrachten die de studenten krijgen zijn in de lessen meestal klein om met kleine stappen veel te leren. In een jaar kan je heel veel bereiken met alle kennis die je opgeleverd krijgt in dat jaar. Dat is duidelijk te zien aan de website die is gemaakt door onze studenten. Software Development levert zeker ook veel baanmogelijkheden doordat de toekomst vol zit met technologische apparaten. Daarom zijn er ook heel veel werknemers nodig en beschikbaar om dat waar te kunnen maken. </p>
+				</div>
+				<div class="header">
+                    <h2>De programmeertalen</h2>
+                    <p>De programmeertalen die je in het eerste jaar leert zijn: HTML, CSS, PHP, C#, Python en Javascript. De talen worden langzaam door het jaar heen geleerd en zijn makkelijk te begrijpen als je ze leert. </p>
+                </div>
+				<div class="header">
+					<h2>De blokken</h2>
+					<p>Elk jaar is verdeeld over 2 halve jaren die blokken worden genoemd. Na het eerste jaar wordt er bij het besluiten van jouw overgang of er een mogelijkheid is om te versnellen. Versnellen betekend dat je de opleiding in 3 jaar afrond in plaats van 4. Het 2e blok van jaar 2 is het stageblok dat BPV wordt genoemd. Daarin ga je stage lopen bij een bedrijf waar je gaat werken als Software Developer</p>
+				</div>
+            </header>
+            <main>
+                <div class="main">
+                    <h1>Meer informatie lezen en contact opnemen kan allemaal op de website.</h1>
+                    <a href="">Bekijk informatie</a>
+                </div>
+                <div class="main2">
+                    <h2>Na de opleiding</h2>
+					<p>Na de opleiding kan je naar het HBO indien je goed genoeg bent afgestudeerd. Ook zijn er baan mogelijkheden om als Software Developer te kunnen werken. Er zijn namelijk best veel werknemers nodig om veel technologie mogelijkheden waar te kunnen maken.</p>
+                </div>
+				<div class="main2">
+					<h2>Vakanties</h2>
+					<p>Op de opleiding zijn de vakanties redelijk hetzelfde als andere kinderen op andere scholen. Af en toe heb je 1 of 2 weken eerder vakantie vanwege bufferweken. Bufferweken kunnen gezien worden als vakanties maar ook als werkweken. In een bufferweek maak je alles aan werk dat nog bij gewerkt moet worden. Als er wordt aangegeven dat dat niet hoeft heb je in de bufferweek niets te doen en heb je dus eigenlijk vakantie.</p>
+				</div>
+            </main>
+            <footer>
+                <p>&copy; 2022 Jeremy de Leeuw - student bij Curio.</p>
+            </footer>
+        </div>
+    </body>
 </html>
